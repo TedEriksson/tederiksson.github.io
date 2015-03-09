24ed9fdf5bb1a4e50d2a0addcecf34cf981386e0
Ted <ted.eriksson@conjure.co.uk>
2015-03-09 21:35:24 +0000

    add api key

--- blog-loader.js
+++ blog-loader.js
@@ -1,5 +1,5 @@
 $(document).ready( function () {
-	$.getJSON("https://www.googleapis.com/blogger/v3/blogs/1230791007767001499/posts", function (data) {
+	$.getJSON("https://www.googleapis.com/blogger/v3/blogs/1230791007767001499/posts?key=AIzaSyAD_gpKq2azxvP7_iejW8uU8ok6P6lWIjc", function (data) {
 		console.log(data.items.content);
 	})
 });
